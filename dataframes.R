ea11a73c340e16fb25c4efff82a2dec24e845062
Thinula Damsith <thinula_03@icloud.com>
2023-09-19 22:12:56 +0530

    Data Frames

---
+++ dataframes.R
@@ -0,0 +1,8 @@
+sample <- data.frame(
+    Machine_ID = c("E001", "E002", "E003"),
+    Machine_Name = c("Suzuki", "Mitsubishi", "Deck"),
+    Production_QTY = c(576, 436, 392),
+    Profit = c(766.34, 432.11, 543.21)
+)
+print(sample)#print only the table
+print(summary(sample))#print the summary of the dataset
