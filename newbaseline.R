977cea67533fa138434f63a96bc56d613dd1f694
Thinula Damsith <thinula_03@icloud.com>
2023-09-20 09:53:20 +0530

    baseline.R after bug fixing

--- newbaseline.R
+++ newbaseline.R
@@ -1,9 +1,9 @@
-install.packages("ggplot")
-library(ggplot)
+install.packages("ggplot2")
+library(ggplot2)
 data <- read.csv("/Users/thinuladamsith/Downloads/Datasets/longevity.csv")
+data
 print(data)
 summary(data)
-
-ggplot(data,aes(x = AgeAtDeath, fill = factor(smokes))) +
-  geom_density() +
-  facet_grid(smokes~.)
+ggplot(data, aes(x=AgeAtDeath, fill=factor(Smokes))) + 
+  geom_density()+
+  facet_grid(Smokes~.)
