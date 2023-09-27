3150e993de7d2e3bf82cd771311cac1a0003744f
Thinula Damsith <thinula_03@icloud.com>
2023-09-27 11:50:56 +0530

    Histograms

---
+++ histogram2.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+?mpg
+
+dataset <- mpg
+View(dataset)
+
+head(dataset)
+tail(dataset)
+
+summary(dataset)
+
+car_model_frequencies <- table(dataset$manufacturer)
+car_model_frequencies
+
+barplot(car_model_frequencies,
+        main = "Histogram - manufacturer & Frequency",
+        xlab = "Manufacturers",
+        ylab = "Frequency",
+        col = "skyblue")
--- tidyverse.R
+++ tidyverse.R
@@ -10,4 +10,7 @@ tail(dataset)
 
 summary(dataset)
 
-hist(dataset$cty)
+hist(dataset$cty, main = "Histogram on fuel consumption in the city area",
+     xlab = "Fuel Consumption",
+     ylab = "Frequency of cars",
+     col = "red")
